b0e3eaf4246a7e49f99db346078a6e638fbdb9c2
Laxmikant-Kulkarni <lvkulkarni2000@gmail.com>
2023-04-15 13:23:39 +0200

    Committing .gitignore and DataTypes.java

---
+++ JavaSEBasicsToAdvanced/src/na/java/datatypes/DataTypes.java
@@ -0,0 +1,11 @@
+package na.java.datatypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello TO Data Types in Java");
+
+	}
+
+}
